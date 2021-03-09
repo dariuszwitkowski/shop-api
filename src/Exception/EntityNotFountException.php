e8ab7f585bc6c33c7433bd5b725136433d5fd883
witkowskid <dariuszwitkowski@brandnova.com>
2021-03-09 02:10:07 +0100

    Add basic api exceptions

---
+++ src/Exception/EntityNotFountException.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace App\Exception;
+
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\Response;
+
+class EntityNotFountException extends \RuntimeException
+{
+    const ERROR_MESSAGE = "Requested entity not found";
+
+    public function __construct()
+    {
+        parent::__construct(self::ERROR_MESSAGE, Response::HTTP_NOT_FOUND);
+    }
+}
